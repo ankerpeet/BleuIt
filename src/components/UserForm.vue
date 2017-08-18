af6ec872fe2004bd97d8850fc4ffc4b6d9853605
Connor Burnett <conb419@gmail.com>
2017-08-18 11:48:45 -0600

    first commit bleuit front end, lots of stuff

---
+++ src/components/UserForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="userform">
+    <form @click="createUser">
+      <input type="text" v-model="username" placeholder="username">
+      <input type="text" v-model="password" placeholder="password">
+      <input type="text" v-model="email" placeholder="email">
+      <button type="submit">CREATE</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { store } from '../store'
+
+export default {
+  name: 'userform',
+  data () {
+    return {
+      username: '',
+      password: '',
+      email: '',
+      results: ''
+    }
+  },
+  methods: {
+    createUser() {
+      store.createUser(this.username, this.password, this.email)
+        .then(res => {
+          this.results = res
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import $ from 'jquery'
+
+var state = {
+    user: {
+        username: '',
+        userId: ''
+    },
+    
+}
+
+let store = {
+    createUser(username, password, email) {
+        //Get response from server
+        //If return true, we're good.
+        state.user.username = username
+        //state.user.userId = userId
+    }
+}
